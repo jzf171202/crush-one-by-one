eb5ce3ddb9ced8a6580c9ec1098246a4ceaaddf9
jinzifu <jinzifu123@163.com>
2018-11-12 23:59:04 +0800

    依赖第三方recycleview封装库

--- app/src/main/java/com/zjrb/sjzsw/adapter/IMultiItemType.java
+++
@@ -1,7 +0,0 @@
-package com.zjrb.sjzsw.adapter;
-
-public interface IMultiItemType<T> {
-    int getLayoutId(int itemType);
-
-    int getItemViewType(int position, T t);
-}
--- app/src/main/java/com/zjrb/sjzsw/ui/fragment/RecycleFragment.java
+++ app/src/main/java/com/zjrb/sjzsw/ui/fragment/RecycleFragment.java
@@ -4,17 +4,19 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.andview.refreshview.XRefreshView;
 import com.bumptech.glide.Glide;
+import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 import com.youth.banner.loader.ImageLoader;
+import com.zhy.adapter.recyclerview.wrapper.HeaderAndFooterWrapper;
 import com.zjrb.sjzsw.R;
-import com.zjrb.sjzsw.adapter.BaseListAdapter;
-import com.zjrb.sjzsw.adapter.DividerGridItemDecoration;
-import com.zjrb.sjzsw.adapter.HomeImgAdapter;
+import com.zjrb.sjzsw.adapter.HomeTestAdapter;
 import com.zjrb.sjzsw.databinding.FrRecycleBinding;
 import com.zjrb.sjzsw.model.GirlsItemModel;
 
@@ -24,34 +26,16 @@ import java.util.List;
 
 public class RecycleFragment extends BaseFragment<FrRecycleBinding> {
     private String url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOlt6QXr6EtRYnCyKBUasvHhGj_-xnseBtgeDKj6Rjl-7uP3M3";
-
-    private List<GirlsItemModel> girlsItemModels = new ArrayList<>(Arrays.asList(
-            new GirlsItemModel(1, "1", url),
-            new GirlsItemModel(2, "2", url),
-            new GirlsItemModel(3, "3", url),
-            new GirlsItemModel(4, "4", url),
-            new GirlsItemModel(5, "5", url),
-            new GirlsItemModel(6, "6", url),
-            new GirlsItemModel(7, "7", url),
-            new GirlsItemModel(8, "8", url),
-            new GirlsItemModel(9, "9", url),
-            new GirlsItemModel(10, "10", url),
-            new GirlsItemModel(10, "14", url),
-            new GirlsItemModel(10, "15", url),
-            new GirlsItemModel(10, "16", url),
-            new GirlsItemModel(10, "17", url),
-            new GirlsItemModel(10, "18", url),
-            new GirlsItemModel(10, "19", url),
-            new GirlsItemModel(10, "20", url),
-            new GirlsItemModel(10, "21", url),
-            new GirlsItemModel(10, "22", url),
-            new GirlsItemModel(11, "23", url),
-            new GirlsItemModel(12, "24", url),
-            new GirlsItemModel(13, "13", url)
+    private final static int SPAN_COUNT = 2;
+    private List<GirlsItemModel> itemModels = new ArrayList<>(Arrays.asList(
+            new GirlsItemModel(1, "1", url, 0),
+            new GirlsItemModel(2, "2", url, 1),
+            new GirlsItemModel(3, "3", url, 0),
+            new GirlsItemModel(4, "4", url, 1)
     ));
+    private List<GirlsItemModel> list = new ArrayList<>();
+    private HomeTestAdapter homeTestAdapter;
 
-    private BaseListAdapter listAdapter;
-    private HomeImgAdapter homeImgAdapter;
 
     @Override
     protected int getLayoutId() {
@@ -62,57 +46,71 @@ public class RecycleFragment extends BaseFragment<FrRecycleBinding> {
     protected void init(@Nullable Bundle savedInstanceState) {
     }
 
-//    private void setListAdapter() {
-//        t.listview.setAdapter(listAdapter = new BaseListAdapter<GirlsItemModel>(
-//                getActivity(), girlsItemModels, R.layout.item_homelist) {
-//            @Override
-//            public void convert(ListViewHolder viewHolder, GirlsItemModel listItemEntity) {
-//                TextView name = viewHolder.getView(R.id.name);
-//                ImageView img = viewHolder.getView(R.id.img);
-//                name.setText("" + listItemEntity.getName());
-//                Glide.with(context).load(listItemEntity.getUrl()).placeholder(R.drawable.icon_simple).into(img);
-//            }
-//        });
-//    }
-
     @Override
     protected void initView(View root) {
-//        t.recycleview.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
-//        t.recycleview.addItemDecoration(new DividerListItemDecoration(getActivity(), DividerListItemDecoration.HORIZONTAL_LIST, R.drawable.recycle_divider))
-        t.recycleview.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-        t.recycleview.addItemDecoration(new DividerGridItemDecoration(getActivity(), R.drawable.recycle_divider));
-        t.recycleview.setAdapter(homeImgAdapter = new HomeImgAdapter(getActivity(), R.layout.item_homelist, girlsItemModels));
+        list.addAll(itemModels);
         initXrefreshView();
-        initBanner();
+        // setNormalAdapter();
+        setHeadAdapter();
+    }
+
+    private void setHeadAdapter() {
+        t.recycleview.setLayoutManager(new GridLayoutManager(getActivity(), SPAN_COUNT));
+        // t.recycleview.addItemDecoration(new DividerGridItemDecoration(getActivity(), R.drawable.recycle_divider));
+
+        HeaderAndFooterWrapper headerAndFooterWrapper = new HeaderAndFooterWrapper(
+                homeTestAdapter = new HomeTestAdapter(getActivity(), R.layout.item_homelist, list));
+        View head = LayoutInflater.from(context).inflate(R.layout.head_recycleview, null, false);
+        initBanner(head);
+        headerAndFooterWrapper.addHeaderView(head);
+        t.recycleview.setAdapter(headerAndFooterWrapper);
+        headerAndFooterWrapper.notifyDataSetChanged();
+    }
+
+    private void setNormalAdapter() {
+        //线性纵向列表
+//        t.recycleview.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+//        t.recycleview.addItemDecoration(new DividerListItemDecoration(getActivity(), DividerListItemDecoration.VERTICAL_LIST, R.drawable.recycle_divider));
+        //网格列表
+        t.recycleview.setLayoutManager(new GridLayoutManager(getActivity(), SPAN_COUNT));
+        //t.recycleview.addItemDecoration(new DividerGridItemDecoration(getActivity(), R.drawable.recycle_divider));
+        t.recycleview.setAdapter(homeTestAdapter = new HomeTestAdapter(getActivity(), R.layout.item_homelist, list));
     }
 
     private void initXrefreshView() {
         t.xrefreshview.setAutoRefresh(true);
         t.xrefreshview.setPullLoadEnable(true);
         t.xrefreshview.setPullRefreshEnable(true);
+        t.xrefreshview.setAutoLoadMore(true);
         t.xrefreshview.setMoveForHorizontal(true);
         t.xrefreshview.setXRefreshViewListener(new XRefreshView.SimpleXRefreshListener() {
             @Override
             public void onRefresh(boolean isPullDown) {
                 super.onRefresh(isPullDown);
-                t.xrefreshview.stopRefresh();
+                t.xrefreshview.postDelayed(() -> t.xrefreshview.stopRefresh(), 1000);
             }
 
             @Override
             public void onLoadMore(boolean isSilence) {
                 super.onLoadMore(isSilence);
-                t.xrefreshview.stopLoadMore();
-//                girlsItemModels.addAll(girlsItemModels);
-//                homeImgAdapter.notifyDataSetChanged();
+                list.addAll(itemModels);
+                if (homeTestAdapter != null) {
+                    // TODO: 2018/11/12  数据刷新无效
+                    homeTestAdapter.notifyDataSetChanged();
+                }
+                t.xrefreshview.postDelayed(() -> t.xrefreshview.stopLoadMore(), 1000);
             }
         });
     }
 
-    private void initBanner() {
-        t.banner.setImages(girlsItemModels);
-        t.banner.isAutoPlay(true);
-        t.banner.setDelayTime(4000);
-        t.banner.setImageLoader(new ImageLoader() {
+    private void initBanner(View head) {
+        Banner banner = head.findViewById(R.id.banner);
+        TextView title = head.findViewById(R.id.mid_text);
+        title.setOnClickListener(v -> showToast("你点击了head的标题"));
+        banner.setImages(itemModels);
+        banner.isAutoPlay(true);
+        banner.setDelayTime(3000);
+        banner.setImageLoader(new ImageLoader() {
             @Override
             public void displayImage(Context context, Object object, ImageView imageView) {
                 GirlsItemModel girlsItemModel = (GirlsItemModel) object;
@@ -121,7 +119,7 @@ public class RecycleFragment extends BaseFragment<FrRecycleBinding> {
                 }
             }
         });
-        t.banner.setIndicatorGravity(BannerConfig.RIGHT);
-        t.banner.start();
+        banner.setIndicatorGravity(BannerConfig.RIGHT);
+        banner.start();
     }
 }
