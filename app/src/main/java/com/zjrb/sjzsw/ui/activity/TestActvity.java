5a8980ba7f69bdde056fae44f2c9813034d71d24
jinzifu <jinzifu123@163.com>
2019-02-06 20:16:49 +0800

    Canvas和Paint用法

--- app/src/main/java/com/zjrb/sjzsw/ui/activity/TestActvity.java
+++ app/src/main/java/com/zjrb/sjzsw/ui/activity/TestActvity.java
@@ -1,44 +1,50 @@
 package com.zjrb.sjzsw.ui.activity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.zjrb.sjzsw.R;
-import com.zjrb.sjzsw.biz.other.IProxy;
-import com.zjrb.sjzsw.databinding.AcTestBinding;
-import com.zjrb.sjzsw.test.MyInvocationHandler;
-import com.zjrb.sjzsw.test.MyProxy;
-
-import java.lang.reflect.Proxy;
-
-public class TestActvity extends BaseActivity<AcTestBinding> implements View.OnClickListener {
-
+public class TestActvity extends AppCompatActivity {
     @Override
-    protected int getLayoutId() {
-        return R.layout.ac_test;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new CustomerView(this));
     }
 
-    @Override
-    protected void init(Bundle savedInstanceState) {
-        t.test.setOnClickListener(this);
-    }
+    class CustomerView extends View {
+        Paint paint1, paint2, paint3;
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.test:
-                testProxy();
-                break;
+        public CustomerView(Context context) {
+            super(context);
+            paint1 = new Paint();//圆饼画笔
+            paint1.setAntiAlias(true);//抗锯齿
+            paint1.setStrokeWidth(2);//画笔宽度
+            paint1.setColor(Color.RED);//画笔颜色
+
+            paint2 = new Paint();//圆心画笔
+            paint2.setAntiAlias(true);
+            paint2.setColor(Color.YELLOW);
+
+            paint3 = new Paint();//文本画笔
+            paint3.setAntiAlias(true);
+            paint3.setTextSize(30);//文本大小
+            paint3.setColor(Color.WHITE);
         }
-    }
 
-    private void testProxy() {
-        MyProxy myProxy = new MyProxy();
-        //ClassLoader 每次生成动态代理类对象时都需要指定一个类装载器对象
-        IProxy iProxy = (IProxy) Proxy.newProxyInstance(
-                Thread.currentThread().getContextClassLoader(),
-                myProxy.getClass().getInterfaces(),
-                new MyInvocationHandler(myProxy));
-        iProxy.print();
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            //绘制圆饼
+            canvas.drawCircle(300, 300, 200, paint1);
+            //绘制圆心
+            canvas.drawCircle(300, 300, 10, paint2);
+            //绘制文本
+            canvas.drawText("圆心", 320, 310, paint3);
+        }
     }
 }
